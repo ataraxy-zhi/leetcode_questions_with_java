df49c3520b42d470701deedbb2a26c424199573b
ataraxy-zhi <2711059631@qq.com>
2021-03-01 14:00:04 +0800

    commit

--- src/com/solution/simple/addbinary67/AddBinary.java
+++ src/com/solution/simple/addbinary67/AddBinary.java
@@ -2,20 +2,19 @@ package com.solution.simple.addbinary67;
 
 /**
  * @className: AddBinary
- * @description:
- * 给定两个二进制字符串，返回他们的和（用二进制表示）。
- *
+ * @description: 给定两个二进制字符串，返回他们的和（用二进制表示）。
+ * <p>
  * 输入为非空字符串且只包含数字 1 和 0。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: a = "11", b = "1"
  * 输出: "100"
  * 示例 2:
- *
+ * <p>
  * 输入: a = "1010", b = "1011"
  * 输出: "10101"
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/add-binary
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -26,57 +25,57 @@ public class AddBinary {
 
     public static void main(String[] args) {
 
-        String str1="110";
-        String str2="01";
-        System.out.println(addBinary(str1,str2));
+        String str1 = "110";
+        String str2 = "01";
+        System.out.println(addBinary(str1, str2));
     }
 
     public static String addBinary(String a, String b) {
 
-        if(a.length()==0){
+        if (a.length() == 0) {
             return b;
         }
 
-        if(b.length()==0){
+        if (b.length() == 0) {
             return a;
         }
 
         //按照一般习惯：大数在前
         String temps;
-        if(a.length()<b.length()){
-            temps=a;
-            a=b;
-            b=temps;
+        if (a.length() < b.length()) {
+            temps = a;
+            a = b;
+            b = temps;
         }
 
         //数位本位和
-        int sum=0;
+        int sum = 0;
 
         //数位进位
-        int carry=0;
+        int carry = 0;
 
         //这里使用StringBuilder而不使用String，是为了便于添加数据
-        StringBuilder results=new StringBuilder();
+        StringBuilder results = new StringBuilder();
 
-        for(int i=a.length()-1,j=b.length()-1;i>=0 || j>=0;i--,j--){
+        for (int i = a.length() - 1, j = b.length() - 1; i >= 0 || j >= 0; i--, j--) {
             //数位之和
-            int sumTemp=0;
+            int sumTemp = 0;
 
-            if(j>=0){
-                sumTemp+=b.charAt(j)-'0';
+            if (j >= 0) {
+                sumTemp += b.charAt(j) - '0';
             }
 
-            sumTemp+=a.charAt(i)-'0';
+            sumTemp += a.charAt(i) - '0';
 
-            sum=(sumTemp+carry)%2;
-            carry=(sumTemp+carry)/2;
+            sum = (sumTemp + carry) % 2;
+            carry = (sumTemp + carry) / 2;
 
             results.append(sum);
 
         }
 
-        //最高位进位不为0
-        if(carry>0){
+        // 最高位进位不为0
+        if (carry > 0) {
             results.append(carry);
         }
 
--- src/com/solution/simple/reverseinteger7/ReverseInteger.java
+++ src/com/solution/simple/reverseinteger7/ReverseInteger.java
@@ -1,25 +1,24 @@
 package com.solution.simple.reverseinteger7;
 
 /**
- * @description:
- * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
- *
+ * @description: 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: 123
  * 输出: 321
  *  示例 2:
- *
+ * <p>
  * 输入: -123
  * 输出: -321
  * 示例 3:
- *
+ * <p>
  * 输入: 120
  * 输出: 21
  * 注意:
- *
+ * <p>
  * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/reverse-integer
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -33,23 +32,23 @@ public class ReverseInteger {
 
     public static int reverse(int x) {
 
-        if(x>=0 && x<=9){
+        if (x >= 0 && x <= 9) {
             return x;
         }
 
-        long result=0;
+        long result = 0;
 
         //拆分数字并计算结果
-        while(x!=0){
-            result=result*10+x%10;
-            x/=10;
+        while (x != 0) {
+            result = result * 10 + x % 10;
+            x /= 10;
         }
 
         //判断是否溢出
-        if(result>Math.pow(2,31)-1 || result<-Math.pow(2,31)){
+        if (result > Math.pow(2, 31) - 1 || result < -Math.pow(2, 31)) {
             return 0;
-        }else{
-            return (int)result;
+        } else {
+            return (int) result;
         }
 
 
