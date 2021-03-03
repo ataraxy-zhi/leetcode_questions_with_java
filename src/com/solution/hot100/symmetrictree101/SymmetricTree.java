510f14ced341e6881eabe223b71aa53e48a9534f
ataraxy-zhi <2711059631@qq.com>
2021-03-03 13:51:32 +0800

    commit

---
+++ src/com/solution/hot100/symmetrictree101/SymmetricTree.java
@@ -0,0 +1,107 @@
+package com.solution.hot100.symmetrictree101;
+
+/**
+ * @ClassName SymmetricTree
+ * @Description 101. Symmetric Tree
+ * Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * <p>
+ * Input: root = [1,2,2,3,4,4,3]
+ * Output: true
+ * Example 2:
+ * <p>
+ * <p>
+ * Input: root = [1,2,2,null,3,null,3]
+ * Output: false
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * The number of nodes in the tree is in the range [1, 1000].
+ * -100 <= Node.val <= 100
+ * <p>
+ * <p>
+ * Follow up: Could you solve it both recursively and iteratively?
+ * <p>
+ * <p>
+ * <p>
+ * 101. 对称二叉树
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ * <p>
+ * <p>
+ * <p>
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * / \ / \
+ * 3  4 4  3
+ * <p>
+ * <p>
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * \   \
+ * 3    3
+ * <p>
+ * <p>
+ * 进阶：
+ * <p>
+ * 你可以运用递归和迭代两种方法解决这个问题吗？
+ * @Author
+ * @Date 2021/3/3 11:55
+ */
+public class SymmetricTree {
+
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static class Solution {
+        public boolean isSymmetric(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+
+            return isSame(root.left, root.right);
+        }
+
+        public boolean isSame(TreeNode left, TreeNode right) {
+            // 左右子树同时为空
+            if (left == null && right == null) {
+                return true;
+            }
+
+            // 左右子树相等
+            if (left != null && right != null && left.val == right.val) {
+                return isSame(left.left, right.right) && isSame(left.right, right.left);
+            } else {
+                return false;
+            }
+        }
+
+    }
+}
