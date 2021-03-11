37c979ec78eec1349dc809f62694470818129fb5
ataraxy-zhi <2711059631@qq.com>
2021-03-11 22:28:32 +0800

    227. Basic Calculator II

---
+++ src/com/solution/onequestionperday/march2021/basiccalculatorii227/BasicCalculatorII.java
@@ -0,0 +1,111 @@
+package com.solution.onequestionperday.march2021.basiccalculatorii227;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @ClassName BasicCalculatorII
+ * @Description 227. Basic Calculator II
+ * Given a string s which represents an expression, evaluate this expression and return its value.
+ * <p>
+ * The integer division should truncate toward zero.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "3+2*2"
+ * Output: 7
+ * Example 2:
+ * <p>
+ * Input: s = " 3/2 "
+ * Output: 1
+ * Example 3:
+ * <p>
+ * Input: s = " 3+5 / 2 "
+ * Output: 5
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= s.length <= 3 * 105
+ * s consists of integers and operators ('+', '-', '*', '/') separated by some number of spaces.
+ * s represents a valid expression.
+ * All the integers in the expression are non-negative integers in the range [0, 231 - 1].
+ * The answer is guaranteed to fit in a 32-bit integer.
+ * <p>
+ * <p>
+ * 227. 基本计算器 II
+ * 给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值。
+ * <p>
+ * 整数除法仅保留整数部分。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "3+2*2"
+ * 输出：7
+ * 示例 2：
+ * <p>
+ * 输入：s = " 3/2 "
+ * 输出：1
+ * 示例 3：
+ * <p>
+ * 输入：s = " 3+5 / 2 "
+ * 输出：5
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 3 * 105
+ * s 由整数和算符 ('+', '-', '*', '/') 组成，中间由一些空格隔开
+ * s 表示一个 有效表达式
+ * 表达式中的所有整数都是非负整数，且在范围 [0, 231 - 1] 内
+ * 题目数据保证答案是一个 32-bit 整数
+ * <p>
+ * <p>
+ * <p>
+ * https://leetcode-cn.com/problems/basic-calculator-ii/
+ * @Author
+ * @Date 2021/3/11 22:20
+ */
+public class BasicCalculatorII {
+}
+
+
+class Solution {
+    public int calculate(String s) {
+        Deque<Integer> stack = new LinkedList<Integer>();
+        char preSign = '+';
+        int num = 0;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            if (Character.isDigit(s.charAt(i))) {
+                num = num * 10 + s.charAt(i) - '0';
+            }
+            if (!Character.isDigit(s.charAt(i)) && s.charAt(i) != ' ' || i == n - 1) {
+                switch (preSign) {
+                    case '+':
+                        stack.push(num);
+                        break;
+                    case '-':
+                        stack.push(-num);
+                        break;
+                    case '*':
+                        stack.push(stack.pop() * num);
+                        break;
+                    default:
+                        stack.push(stack.pop() / num);
+                }
+                preSign = s.charAt(i);
+                num = 0;
+            }
+        }
+        int ans = 0;
+        while (!stack.isEmpty()) {
+            ans += stack.pop();
+        }
+        return ans;
+    }
+}
