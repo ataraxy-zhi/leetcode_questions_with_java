10b04a26c8a2ea9807baf1850b00779098524619
ataraxy-zhi <2711059631@qq.com>
2021-03-22 10:57:00 +0800

    剑指 Offer 21. 调整数组顺序使奇数位于偶数前面

--- src/com/solution/pointswordtooffer/psto21/Psto21.java
+++ src/com/solution/pointswordtooffer/psto21/Psto21.java
@@ -1,8 +1,28 @@
-package com.solution.pointswordtooffer.pstto21;
+package com.solution.pointswordtooffer.psto21;
 
 /**
  * @ClassName Psto21
- * @Description
+ * @Description 剑指 Offer 21. 调整数组顺序使奇数位于偶数前面
+ * <p>
+ * 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有奇数位于数组的前半部分，所有偶数位于数组的后半部分。
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：nums = [1,2,3,4]
+ * 输出：[1,3,2,4]
+ * 注：[3,1,2,4] 也是正确的答案之一。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= nums.length <= 50000
+ * 1 <= nums[i] <= 10000
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  * @Author yi
  * @Date 2021/3/22 9:42
  */
