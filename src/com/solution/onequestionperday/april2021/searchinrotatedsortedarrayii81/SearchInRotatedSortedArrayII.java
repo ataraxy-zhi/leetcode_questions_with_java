fbacdcd1c59e1b90a5bae32bf60cd349daebd9fe
ataraxy-zhi <2711059631@qq.com>
2021-04-07 08:58:28 +0800

    81. Search in Rotated Sorted Array II

---
+++ src/com/solution/onequestionperday/april2021/searchinrotatedsortedarrayii81/SearchInRotatedSortedArrayII.java
@@ -0,0 +1,115 @@
+package com.solution.onequestionperday.april2021.searchinrotatedsortedarrayii81;
+
+/**
+ * @ClassName SearchInRotatedSortedArrayII
+ * @Description 81. Search in Rotated Sorted Array II
+ * There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+ * <p>
+ * Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+ * <p>
+ * Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [2,5,6,0,0,1,2], target = 0
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input: nums = [2,5,6,0,0,1,2], target = 3
+ * Output: false
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= nums.length <= 5000
+ * -104 <= nums[i] <= 104
+ * nums is guaranteed to be rotated at some pivot.
+ * -104 <= target <= 104
+ * <p>
+ * <p>
+ * Follow up: This problem is the same as Search in Rotated Sorted Array, where nums may contain duplicates. Would this affect the runtime complexity? How and why?
+ * <p>
+ * 81. 搜索旋转排序数组 II
+ * 已知存在一个按非降序排列的整数数组 nums ，数组中的值不必互不相同。
+ * <p>
+ * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转 ，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,4,4,5,6,6,7] 在下标 5 处经旋转后可能变为 [4,5,6,6,7,0,1,2,4,4] 。
+ * <p>
+ * 给你 旋转后 的数组 nums 和一个整数 target ，请你编写一个函数来判断给定的目标值是否存在于数组中。如果 nums 中存在这个目标值 target ，则返回 true ，否则返回 false 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [2,5,6,0,0,1,2], target = 0
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * 输入：nums = [2,5,6,0,0,1,2], target = 3
+ * 输出：false
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 5000
+ * -104 <= nums[i] <= 104
+ * 题目数据保证 nums 在预先未知的某个下标上进行了旋转
+ * -104 <= target <= 104
+ *  
+ * <p>
+ * 进阶：
+ * <p>
+ * 这是 搜索旋转排序数组 的延伸题目，本题中的 nums  可能包含重复元素。
+ * 这会影响到程序的时间复杂度吗？会有怎样的影响，为什么？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-in-rotated-sorted-array-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author yi
+ * @Date 2021/4/7 8:54
+ */
+public class SearchInRotatedSortedArrayII {
+}
+
+/**
+ * 二分查找
+ */
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return false;
+        }
+        if (n == 1) {
+            return nums[0] == target;
+        }
+
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            if (nums[l] == nums[mid] && nums[mid] == nums[r]) {
+                ++l;
+                --r;
+            } else if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
