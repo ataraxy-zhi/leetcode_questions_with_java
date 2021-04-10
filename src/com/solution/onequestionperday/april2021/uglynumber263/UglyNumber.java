16c2f15c7ecc587a275c60be12946e6c8f80fc2d
ataraxy-zhi <2711059631@qq.com>
2021-04-10 19:19:57 +0800

    263. Ugly Number

---
+++ src/com/solution/onequestionperday/april2021/uglynumber263/UglyNumber.java
@@ -0,0 +1,79 @@
+package com.solution.onequestionperday.april2021.uglynumber263;
+
+/**
+ * @ClassName UglyNumber
+ * @Description 263. Ugly Number
+ * Given an integer n, return true if n is an ugly number.
+ * <p>
+ * Ugly number is a positive number whose prime factors only include 2, 3, and/or 5.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 6
+ * Output: true
+ * Explanation: 6 = 2 × 3
+ * Example 2:
+ * <p>
+ * Input: n = 8
+ * Output: true
+ * Explanation: 8 = 2 × 2 × 2
+ * Example 3:
+ * <p>
+ * Input: n = 14
+ * Output: false
+ * Explanation: 14 is not ugly since it includes another prime factor 7.
+ * Example 4:
+ * <p>
+ * Input: n = 1
+ * Output: true
+ * Explanation: 1 is typically treated as an ugly number.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * -231 <= n <= 231 - 1
+ * <p>
+ * <p>
+ * 263. 丑数
+ * 给你一个整数 n ，请你判断 n 是否为 丑数 。如果是，返回 true ；否则，返回 false 。
+ * <p>
+ * 丑数 就是只包含质因数 2、3 和/或 5 的正整数。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 6
+ * 输出：true
+ * 解释：6 = 2 × 3
+ * 示例 2：
+ * <p>
+ * 输入：n = 8
+ * 输出：true
+ * 解释：8 = 2 × 2 × 2
+ * 示例 3：
+ * <p>
+ * 输入：n = 14
+ * 输出：false
+ * 解释：14 不是丑数，因为它包含了另外一个质因数 7 。
+ * 示例 4：
+ * <p>
+ * 输入：n = 1
+ * 输出：true
+ * 解释：1 通常被视为丑数。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * -231 <= n <= 231 - 1
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/ugly-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author yi
+ * @Date 2021/4/10 19:17
+ */
+public class UglyNumber {
+}
