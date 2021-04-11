a9f8f60bc4858b8df5105dfefc8cdd2ca03b3ff6
ataraxy-zhi <2711059631@qq.com>
2021-04-11 17:39:45 +0800

    264. Ugly Number II

---
+++ src/com/solution/onequestionperday/april2021/uglynumberii264/UglyNumberII.java
@@ -0,0 +1,58 @@
+package com.solution.onequestionperday.april2021.uglynumberii264;
+
+/**
+ * @ClassName UglyNumberII
+ * @Description 264. Ugly Number II
+ * Given an integer n, return the nth ugly number.
+ * <p>
+ * Ugly number is a positive number whose prime factors only include 2, 3, and/or 5.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 10
+ * Output: 12
+ * Explanation: [1, 2, 3, 4, 5, 6, 8, 9, 10, 12] is the sequence of the first 10 ugly numbers.
+ * Example 2:
+ * <p>
+ * Input: n = 1
+ * Output: 1
+ * Explanation: 1 is typically treated as an ugly number.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= n <= 1690
+ * <p>
+ * 264. 丑数 II
+ * 给你一个整数 n ，请你找出并返回第 n 个 丑数 。
+ * <p>
+ * 丑数 就是只包含质因数 2、3 和/或 5 的正整数。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 10
+ * 输出：12
+ * 解释：[1, 2, 3, 4, 5, 6, 8, 9, 10, 12] 是由前 10 个丑数组成的序列。
+ * 示例 2：
+ * <p>
+ * 输入：n = 1
+ * 输出：1
+ * 解释：1 通常被视为丑数。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= n <= 1690
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/ugly-number-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author yi
+ * @Date 2021/4/11 17:38
+ */
+public class UglyNumberII {
+}
