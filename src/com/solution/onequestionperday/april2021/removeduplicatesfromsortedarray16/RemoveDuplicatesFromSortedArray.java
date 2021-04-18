308f17306cb101056672b774f38e88cbdb136714
ataraxy-zhi <2711059631@qq.com>
2021-04-18 21:26:19 +0800

    26. Remove Duplicates from Sorted Array

---
+++ src/com/solution/onequestionperday/april2021/removeduplicatesfromsortedarray16/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,117 @@
+package com.solution.onequestionperday.april2021.removeduplicatesfromsortedarray16;
+
+/**
+ * @ClassName RemoveDuplicatesFromSortedArray
+ * @Description 26. Remove Duplicates from Sorted Array
+ * Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+ * <p>
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * <p>
+ * Clarification:
+ * <p>
+ * Confused why the returned value is an integer but your answer is an array?
+ * <p>
+ * Note that the input array is passed in by reference, which means a modification to the input array will be known to the caller as well.
+ * <p>
+ * Internally you can think of this:
+ * <p>
+ * // nums is passed in by reference. (i.e., without making a copy)
+ * int len = removeDuplicates(nums);
+ * <p>
+ * // any modification to nums in your function would be known by the caller.
+ * // using the length returned by your function, it prints the first len elements.
+ * for (int i = 0; i < len; i++) {
+ * print(nums[i]);
+ * }
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,1,2]
+ * Output: 2, nums = [1,2]
+ * Explanation: Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the returned length.
+ * Example 2:
+ * <p>
+ * Input: nums = [0,0,1,1,1,2,2,3,3,4]
+ * Output: 5, nums = [0,1,2,3,4]
+ * Explanation: Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively. It doesn't matter what values are set beyond the returned length.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 0 <= nums.length <= 3 * 104
+ * -104 <= nums[i] <= 104
+ * nums is sorted in ascending order.
+ * <p>
+ * 26. 删除有序数组中的重复项
+ * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+ * <p>
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ * <p>
+ *  
+ * <p>
+ * 说明:
+ * <p>
+ * 为什么返回数值是整数，但输出的答案是数组呢?
+ * <p>
+ * 请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+ * <p>
+ * 你可以想象内部操作如下:
+ * <p>
+ * // nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+ * int len = removeDuplicates(nums);
+ * <p>
+ * // 在函数里修改输入数组对于调用者是可见的。
+ * // 根据你的函数返回的长度, 它会打印出数组中 该长度范围内 的所有元素。
+ * for (int i = 0; i < len; i++) {
+ *     print(nums[i]);
+ * }
+ *  
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,1,2]
+ * 输出：2, nums = [1,2]
+ * 解释：函数应该返回新的长度 2 ，并且原数组 nums 的前两个元素被修改为 1, 2 。不需要考虑数组中超出新长度后面的元素。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [0,0,1,1,1,2,2,3,3,4]
+ * 输出：5, nums = [0,1,2,3,4]
+ * 解释：函数应该返回新的长度 5 ， 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4 。不需要考虑数组中超出新长度后面的元素。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= nums.length <= 3 * 104
+ * -104 <= nums[i] <= 104
+ * nums 已按升序排列
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author yi
+ * @Date 2021/4/18 21:24
+ */
+public class RemoveDuplicatesFromSortedArray {
+}
+
+/**
+ * 双指针
+ */
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int p = 0;
+        int q = 1;
+        while (q < nums.length) {
+            if (nums[p] != nums[q]) {
+                nums[p + 1] = nums[q];
+                p++;
+            }
+            q++;
+        }
+        return p + 1;
+    }
+
+}
