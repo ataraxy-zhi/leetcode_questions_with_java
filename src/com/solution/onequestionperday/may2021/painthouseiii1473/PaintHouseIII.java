caf175581206c27f38b52dc513e7f56d24c910a7
ataraxy-zhi <2711059631@qq.com>
2021-05-04 21:04:20 +0800

    1473. Paint House III

---
+++ src/com/solution/onequestionperday/may2021/painthouseiii1473/PaintHouseIII.java
@@ -0,0 +1,187 @@
+package com.solution.onequestionperday.may2021.painthouseiii1473;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName PaintHouseIII
+ * @Description 1473. Paint House III
+ * There is a row of m houses in a small city, each house must be painted with one of the n colors (labeled from 1 to n), some houses that have been painted last summer should not be painted again.
+ * <p>
+ * A neighborhood is a maximal group of continuous houses that are painted with the same color.
+ * <p>
+ * For example: houses = [1,2,2,3,3,2,1,1] contains 5 neighborhoods [{1}, {2,2}, {3,3}, {2}, {1,1}].
+ * Given an array houses, an m x n matrix cost and an integer target where:
+ * <p>
+ * houses[i]: is the color of the house i, and 0 if the house is not painted yet.
+ * cost[i][j]: is the cost of paint the house i with the color j + 1.
+ * Return the minimum cost of painting all the remaining houses in such a way that there are exactly target neighborhoods. If it is not possible, return -1.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: houses = [0,0,0,0,0], cost = [[1,10],[10,1],[10,1],[1,10],[5,1]], m = 5, n = 2, target = 3
+ * Output: 9
+ * Explanation: Paint houses of this way [1,2,2,1,1]
+ * This array contains target = 3 neighborhoods, [{1}, {2,2}, {1,1}].
+ * Cost of paint all houses (1 + 1 + 1 + 1 + 5) = 9.
+ * Example 2:
+ * <p>
+ * Input: houses = [0,2,1,2,0], cost = [[1,10],[10,1],[10,1],[1,10],[5,1]], m = 5, n = 2, target = 3
+ * Output: 11
+ * Explanation: Some houses are already painted, Paint the houses of this way [2,2,1,2,2]
+ * This array contains target = 3 neighborhoods, [{2,2}, {1}, {2,2}].
+ * Cost of paint the first and last house (10 + 1) = 11.
+ * Example 3:
+ * <p>
+ * Input: houses = [0,0,0,0,0], cost = [[1,10],[10,1],[1,10],[10,1],[1,10]], m = 5, n = 2, target = 5
+ * Output: 5
+ * Example 4:
+ * <p>
+ * Input: houses = [3,1,2,3], cost = [[1,1,1],[1,1,1],[1,1,1],[1,1,1]], m = 4, n = 3, target = 3
+ * Output: -1
+ * Explanation: Houses are already painted with a total of 4 neighborhoods [{3},{1},{2},{3}] different of target = 3.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * m == houses.length == cost.length
+ * n == cost[i].length
+ * 1 <= m <= 100
+ * 1 <= n <= 20
+ * 1 <= target <= m
+ * 0 <= houses[i] <= n
+ * 1 <= cost[i][j] <= 10^4
+ * <p>
+ * 1473. 粉刷房子 III
+ * 在一个小城市里，有 m 个房子排成一排，你需要给每个房子涂上 n 种颜色之一（颜色编号为 1 到 n ）。有的房子去年夏天已经涂过颜色了，所以这些房子不需要被重新涂色。
+ * <p>
+ * 我们将连续相同颜色尽可能多的房子称为一个街区。（比方说 houses = [1,2,2,3,3,2,1,1] ，它包含 5 个街区  [{1}, {2,2}, {3,3}, {2}, {1,1}] 。）
+ * <p>
+ * 给你一个数组 houses ，一个 m * n 的矩阵 cost 和一个整数 target ，其中：
+ * <p>
+ * houses[i]：是第 i 个房子的颜色，0 表示这个房子还没有被涂色。
+ * cost[i][j]：是将第 i 个房子涂成颜色 j+1 的花费。
+ * 请你返回房子涂色方案的最小总花费，使得每个房子都被涂色后，恰好组成 target 个街区。如果没有可用的涂色方案，请返回 -1 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：houses = [0,0,0,0,0], cost = [[1,10],[10,1],[10,1],[1,10],[5,1]], m = 5, n = 2, target = 3
+ * 输出：9
+ * 解释：房子涂色方案为 [1,2,2,1,1]
+ * 此方案包含 target = 3 个街区，分别是 [{1}, {2,2}, {1,1}]。
+ * 涂色的总花费为 (1 + 1 + 1 + 1 + 5) = 9。
+ * 示例 2：
+ * <p>
+ * 输入：houses = [0,2,1,2,0], cost = [[1,10],[10,1],[10,1],[1,10],[5,1]], m = 5, n = 2, target = 3
+ * 输出：11
+ * 解释：有的房子已经被涂色了，在此基础上涂色方案为 [2,2,1,2,2]
+ * 此方案包含 target = 3 个街区，分别是 [{2,2}, {1}, {2,2}]。
+ * 给第一个和最后一个房子涂色的花费为 (10 + 1) = 11。
+ * 示例 3：
+ * <p>
+ * 输入：houses = [0,0,0,0,0], cost = [[1,10],[10,1],[1,10],[10,1],[1,10]], m = 5, n = 2, target = 5
+ * 输出：5
+ * 示例 4：
+ * <p>
+ * 输入：houses = [3,1,2,3], cost = [[1,1,1],[1,1,1],[1,1,1],[1,1,1]], m = 4, n = 3, target = 3
+ * 输出：-1
+ * 解释：房子已经被涂色并组成了 4 个街区，分别是 [{3},{1},{2},{3}] ，无法形成 target = 3 个街区。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * m == houses.length == cost.length
+ * n == cost[i].length
+ * 1 <= m <= 100
+ * 1 <= n <= 20
+ * 1 <= target <= m
+ * 0 <= houses[i] <= n
+ * 1 <= cost[i][j] <= 10^4
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/paint-house-iii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author yi
+ * @Date 2021/5/4 21:02
+ */
+public class PaintHouseIII {
+}
+
+class Solution {
+    // 极大值
+    // 选择 Integer.MAX_VALUE / 2 的原因是防止整数相加溢出
+    static final int INFTY = Integer.MAX_VALUE / 2;
+
+    public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
+        // 将颜色调整为从 0 开始编号，没有被涂色标记为 -1
+        for (int i = 0; i < m; ++i) {
+            --houses[i];
+        }
+
+        // dp 所有元素初始化为极大值
+        int[][][] dp = new int[m][n][target];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                Arrays.fill(dp[i][j], INFTY);
+            }
+        }
+        int[][][] best = new int[m][target][3];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < target; ++j) {
+                best[i][j][0] = best[i][j][2] = INFTY;
+                best[i][j][1] = -1;
+            }
+        }
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (houses[i] != -1 && houses[i] != j) {
+                    continue;
+                }
+
+                for (int k = 0; k < target; ++k) {
+                    if (i == 0) {
+                        if (k == 0) {
+                            dp[i][j][k] = 0;
+                        }
+                    } else {
+                        dp[i][j][k] = dp[i - 1][j][k];
+                        if (k > 0) {
+                            // 使用 best(i-1,k-1) 直接得到 dp(i,j,k) 的值
+                            int first = best[i - 1][k - 1][0];
+                            int firstIdx = best[i - 1][k - 1][1];
+                            int second = best[i - 1][k - 1][2];
+                            dp[i][j][k] = Math.min(dp[i][j][k], (j == firstIdx ? second : first));
+                        }
+                    }
+
+                    if (dp[i][j][k] != INFTY && houses[i] == -1) {
+                        dp[i][j][k] += cost[i][j];
+                    }
+
+                    // 用 dp(i,j,k) 更新 best(i,k)
+                    int first = best[i][k][0];
+                    int firstIdx = best[i][k][1];
+                    int second = best[i][k][2];
+                    if (dp[i][j][k] < first) {
+                        best[i][k][2] = first;
+                        best[i][k][0] = dp[i][j][k];
+                        best[i][k][1] = j;
+                    } else if (dp[i][j][k] < second) {
+                        best[i][k][2] = dp[i][j][k];
+                    }
+                }
+            }
+        }
+
+        int ans = INFTY;
+        for (int j = 0; j < n; ++j) {
+            ans = Math.min(ans, dp[m - 1][j][target - 1]);
+        }
+        return ans == INFTY ? -1 : ans;
+    }
+}
+
