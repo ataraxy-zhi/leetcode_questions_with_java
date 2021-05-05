50a19c59764e0b0706b98a6b4df27546a679c95a
ataraxy-zhi <2711059631@qq.com>
2021-05-05 18:30:56 +0800

    740. Delete and Earn

---
+++ src/com/solution/onequestionperday/may2021/deleteandearn740/DeleteAndEarn.java
@@ -0,0 +1,118 @@
+package com.solution.onequestionperday.may2021.deleteandearn740;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @ClassName DeleteAndEarn
+ * @Description 740. Delete and Earn
+ * Given an array nums of integers, you can perform operations on the array.
+ * <p>
+ * In each operation, you pick any nums[i] and delete it to earn nums[i] points. After, you must delete every element equal to nums[i] - 1 or nums[i] + 1.
+ * <p>
+ * You start with 0 points. Return the maximum number of points you can earn by applying such operations.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [3,4,2]
+ * Output: 6
+ * Explanation: Delete 4 to earn 4 points, consequently 3 is also deleted.
+ * Then, delete 2 to earn 2 points.
+ * 6 total points are earned.
+ * Example 2:
+ * <p>
+ * Input: nums = [2,2,3,3,3,4]
+ * Output: 9
+ * Explanation: Delete 3 to earn 3 points, deleting both 2's and the 4.
+ * Then, delete 3 again to earn 3 points, and 3 again to earn 3 points.
+ * 9 total points are earned.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= nums.length <= 2 * 104
+ * 1 <= nums[i] <= 104
+ * <p>
+ * 740. 删除并获得点数
+ * 给你一个整数数组 nums ，你可以对它进行一些操作。
+ * <p>
+ * 每次操作中，选择任意一个 nums[i] ，删除它并获得 nums[i] 的点数。之后，你必须删除每个等于 nums[i] - 1 或 nums[i] + 1 的元素。
+ * <p>
+ * 开始你拥有 0 个点数。返回你能通过这些操作获得的最大点数。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [3,4,2]
+ * 输出：6
+ * 解释：
+ * 删除 4 获得 4 个点数，因此 3 也被删除。
+ * 之后，删除 2 获得 2 个点数。总共获得 6 个点数。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [2,2,3,3,3,4]
+ * 输出：9
+ * 解释：
+ * 删除 3 获得 3 个点数，接着要删除两个 2 和 4 。
+ * 之后，再次删除 3 获得 3 个点数，再次删除 3 获得 3 个点数。
+ * 总共获得 9 个点数。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 2 * 104
+ * 1 <= nums[i] <= 104
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/delete-and-earn
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author yi
+ * @Date 2021/5/5 18:28
+ */
+public class DeleteAndEarn {
+}
+
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        Arrays.sort(nums);
+        List<Integer> sum = new ArrayList<Integer>();
+        sum.add(nums[0]);
+        int size = 1;
+        for (int i = 1; i < n; ++i) {
+            int val = nums[i];
+            if (val == nums[i - 1]) {
+                sum.set(size - 1, sum.get(size - 1) + val);
+            } else if (val == nums[i - 1] + 1) {
+                sum.add(val);
+                ++size;
+            } else {
+                ans += rob(sum);
+                sum.clear();
+                sum.add(val);
+                size = 1;
+            }
+        }
+        ans += rob(sum);
+        return ans;
+    }
+
+    public int rob(List<Integer> nums) {
+        int size = nums.size();
+        if (size == 1) {
+            return nums.get(0);
+        }
+        int first = nums.get(0), second = Math.max(nums.get(0), nums.get(1));
+        for (int i = 2; i < size; i++) {
+            int temp = second;
+            second = Math.max(first + nums.get(i), second);
+            first = temp;
+        }
+        return second;
+    }
+}
