0e9e3a30745ba0c25963db7a328ab054d2afd2c5
ataraxy-zhi <2711059631@qq.com>
2021-05-09 10:04:38 +0800

    1482. Minimum Number of Days to Make m Bouquets

---
+++ src/com/solution/onequestionperday/may2021/minimumnumberofdaystomakembouquets1482/MinimumNumberOfDaysToMakeMBouquets.java
@@ -0,0 +1,160 @@
+package com.solution.onequestionperday.may2021.minimumnumberofdaystomakembouquets1482;
+
+/**
+ * @ClassName MinimumNumberOfDaysToMakeMBouquets
+ * @Description 1482. Minimum Number of Days to Make m Bouquets
+ * Given an integer array bloomDay, an integer m and an integer k.
+ * <p>
+ * We need to make m bouquets. To make a bouquet, you need to use k adjacent flowers from the garden.
+ * <p>
+ * The garden consists of n flowers, the ith flower will bloom in the bloomDay[i] and then can be used in exactly one bouquet.
+ * <p>
+ * Return the minimum number of days you need to wait to be able to make m bouquets from the garden. If it is impossible to make m bouquets return -1.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: bloomDay = [1,10,3,10,2], m = 3, k = 1
+ * Output: 3
+ * Explanation: Let's see what happened in the first three days. x means flower bloomed and _ means flower didn't bloom in the garden.
+ * We need 3 bouquets each should contain 1 flower.
+ * After day 1: [x, _, _, _, _]   // we can only make one bouquet.
+ * After day 2: [x, _, _, _, x]   // we can only make two bouquets.
+ * After day 3: [x, _, x, _, x]   // we can make 3 bouquets. The answer is 3.
+ * Example 2:
+ * <p>
+ * Input: bloomDay = [1,10,3,10,2], m = 3, k = 2
+ * Output: -1
+ * Explanation: We need 3 bouquets each has 2 flowers, that means we need 6 flowers. We only have 5 flowers so it is impossible to get the needed bouquets and we return -1.
+ * Example 3:
+ * <p>
+ * Input: bloomDay = [7,7,7,7,12,7,7], m = 2, k = 3
+ * Output: 12
+ * Explanation: We need 2 bouquets each should have 3 flowers.
+ * Here's the garden after the 7 and 12 days:
+ * After day 7: [x, x, x, x, _, x, x]
+ * We can make one bouquet of the first three flowers that bloomed. We cannot make another bouquet from the last three flowers that bloomed because they are not adjacent.
+ * After day 12: [x, x, x, x, x, x, x]
+ * It is obvious that we can make two bouquets in different ways.
+ * Example 4:
+ * <p>
+ * Input: bloomDay = [1000000000,1000000000], m = 1, k = 1
+ * Output: 1000000000
+ * Explanation: You need to wait 1000000000 days to have a flower ready for a bouquet.
+ * Example 5:
+ * <p>
+ * Input: bloomDay = [1,10,2,9,3,8,4,7,5,6], m = 4, k = 2
+ * Output: 9
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * bloomDay.length == n
+ * 1 <= n <= 10^5
+ * 1 <= bloomDay[i] <= 10^9
+ * 1 <= m <= 10^6
+ * 1 <= k <= n
+ * <p>
+ * <p>
+ * 1482. 制作 m 束花所需的最少天数
+ * 给你一个整数数组 bloomDay，以及两个整数 m 和 k 。
+ * <p>
+ * 现需要制作 m 束花。制作花束时，需要使用花园中 相邻的 k 朵花 。
+ * <p>
+ * 花园中有 n 朵花，第 i 朵花会在 bloomDay[i] 时盛开，恰好 可以用于 一束 花中。
+ * <p>
+ * 请你返回从花园中摘 m 束花需要等待的最少的天数。如果不能摘到 m 束花则返回 -1 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：bloomDay = [1,10,3,10,2], m = 3, k = 1
+ * 输出：3
+ * 解释：让我们一起观察这三天的花开过程，x 表示花开，而 _ 表示花还未开。
+ * 现在需要制作 3 束花，每束只需要 1 朵。
+ * 1 天后：[x, _, _, _, _]   // 只能制作 1 束花
+ * 2 天后：[x, _, _, _, x]   // 只能制作 2 束花
+ * 3 天后：[x, _, x, _, x]   // 可以制作 3 束花，答案为 3
+ * 示例 2：
+ * <p>
+ * 输入：bloomDay = [1,10,3,10,2], m = 3, k = 2
+ * 输出：-1
+ * 解释：要制作 3 束花，每束需要 2 朵花，也就是一共需要 6 朵花。而花园中只有 5 朵花，无法满足制作要求，返回 -1 。
+ * 示例 3：
+ * <p>
+ * 输入：bloomDay = [7,7,7,7,12,7,7], m = 2, k = 3
+ * 输出：12
+ * 解释：要制作 2 束花，每束需要 3 朵。
+ * 花园在 7 天后和 12 天后的情况如下：
+ * 7 天后：[x, x, x, x, _, x, x]
+ * 可以用前 3 朵盛开的花制作第一束花。但不能使用后 3 朵盛开的花，因为它们不相邻。
+ * 12 天后：[x, x, x, x, x, x, x]
+ * 显然，我们可以用不同的方式制作两束花。
+ * 示例 4：
+ * <p>
+ * 输入：bloomDay = [1000000000,1000000000], m = 1, k = 1
+ * 输出：1000000000
+ * 解释：需要等 1000000000 天才能采到花来制作花束
+ * 示例 5：
+ * <p>
+ * 输入：bloomDay = [1,10,2,9,3,8,4,7,5,6], m = 4, k = 2
+ * 输出：9
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * bloomDay.length == n
+ * 1 <= n <= 10^5
+ * 1 <= bloomDay[i] <= 10^9
+ * 1 <= m <= 10^6
+ * 1 <= k <= n
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-number-of-days-to-make-m-bouquets
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author yi
+ * @Date 2021/5/9 10:01
+ */
+public class MinimumNumberOfDaysToMakeMBouquets {
+}
+
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        if (k * m > bloomDay.length) {
+            return -1;
+        }
+        int low = 1, high = 1;
+        for (int j : bloomDay) {
+            high = Math.max(high, j);
+        }
+        while (low < high) {
+            int days = (high - low) / 2 + low;
+            if (canMake(bloomDay, days, m, k)) {
+                high = days;
+            } else {
+                low = days + 1;
+            }
+        }
+        return low;
+    }
+
+    public boolean canMake(int[] bloomDay, int days, int m, int k) {
+        int bouquets = 0;
+        int flowers = 0;
+        int length = bloomDay.length;
+        for (int i = 0; i < length && bouquets < m; i++) {
+            if (bloomDay[i] <= days) {
+                flowers++;
+                if (flowers == k) {
+                    bouquets++;
+                    flowers = 0;
+                }
+            } else {
+                flowers = 0;
+            }
+        }
+        return bouquets >= m;
+    }
+}
