64b37ea67f0d6eb2863399277bf8cc3ae852045b
ataraxy-zhi <2711059631@qq.com>
2021-05-10 20:27:38 +0800

    872. Leaf-Similar Trees

---
+++ src/com/solution/onequestionperday/may2021/leafsimilartrees872/LeafSimilarTrees.java
@@ -0,0 +1,150 @@
+package com.solution.onequestionperday.may2021.leafsimilartrees872;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName LeafSimilarTrees
+ * @Description 872. Leaf-Similar Trees
+ * Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
+ * <p>
+ * <p>
+ * <p>
+ * For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
+ * <p>
+ * Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+ * <p>
+ * Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * <p>
+ * Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input: root1 = [1], root2 = [1]
+ * Output: true
+ * Example 3:
+ * <p>
+ * Input: root1 = [1], root2 = [2]
+ * Output: false
+ * Example 4:
+ * <p>
+ * Input: root1 = [1,2], root2 = [2,2]
+ * Output: true
+ * Example 5:
+ * <p>
+ * <p>
+ * Input: root1 = [1,2,3], root2 = [1,3,2]
+ * Output: false
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * The number of nodes in each tree will be in the range [1, 200].
+ * Both of the given trees will have values in the range [0, 200].
+ * <p>
+ * <p>
+ * 872. 叶子相似的树
+ * 请考虑一棵二叉树上所有的叶子，这些叶子的值按从左到右的顺序排列形成一个 叶值序列 。
+ * <p>
+ * <p>
+ * <p>
+ * 举个例子，如上图所示，给定一棵叶值序列为 (6, 7, 4, 9, 8) 的树。
+ * <p>
+ * 如果有两棵二叉树的叶值序列是相同，那么我们就认为它们是 叶相似 的。
+ * <p>
+ * 如果给定的两个根结点分别为 root1 和 root2 的树是叶相似的，则返回 true；否则返回 false 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * 输入：root1 = [1], root2 = [1]
+ * 输出：true
+ * 示例 3：
+ * <p>
+ * 输入：root1 = [1], root2 = [2]
+ * 输出：false
+ * 示例 4：
+ * <p>
+ * 输入：root1 = [1,2], root2 = [2,2]
+ * 输出：true
+ * 示例 5：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：root1 = [1,2,3], root2 = [1,3,2]
+ * 输出：false
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 给定的两棵树可能会有 1 到 200 个结点。
+ * 给定的两棵树上的值介于 0 到 200 之间。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/leaf-similar-trees
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author yi
+ * @Date 2021/5/10 20:22
+ */
+public class LeafSimilarTrees {
+}
+
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> seq1 = new ArrayList<Integer>();
+        if (root1 != null) {
+            dfs(root1, seq1);
+        }
+
+        List<Integer> seq2 = new ArrayList<Integer>();
+        if (root2 != null) {
+            dfs(root2, seq2);
+        }
+
+        return seq1.equals(seq2);
+    }
+
+    public void dfs(TreeNode node, List<Integer> seq) {
+        if (node.left == null && node.right == null) {
+            seq.add(node.val);
+        } else {
+            if (node.left != null) {
+                dfs(node.left, seq);
+            }
+            if (node.right != null) {
+                dfs(node.right, seq);
+            }
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
