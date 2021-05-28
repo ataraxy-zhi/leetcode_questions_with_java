3762080c549784a1edddefa9e36047de7f297319
ataraxy-zhi <2711059631@qq.com>
2021-05-28 22:31:02 +0800

    477. Total Hamming Distance

---
+++ src/com/solution/onequestionperday/may2021/totalhammingdistance477/TotalHammingDistance.java
@@ -0,0 +1,73 @@
+package com.solution.onequestionperday.may2021.totalhammingdistance477;
+
+/**
+ * @ClassName TotalHammingDistance
+ * @Description 477. Total Hamming Distance
+ * The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+ * <p>
+ * Given an integer array nums, return the sum of Hamming distances between all the pairs of the integers in nums.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [4,14,2]
+ * Output: 6
+ * Explanation: In binary representation, the 4 is 0100, 14 is 1110, and 2 is 0010 (just
+ * showing the four bits relevant in this case).
+ * The answer will be:
+ * HammingDistance(4, 14) + HammingDistance(4, 2) + HammingDistance(14, 2) = 2 + 2 + 2 = 6.
+ * Example 2:
+ * <p>
+ * Input: nums = [4,14,4]
+ * Output: 4
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= nums.length <= 105
+ * 0 <= nums[i] <= 109
+ * <p>
+ * <p>
+ * 477. 汉明距离总和
+ * 两个整数的 汉明距离 指的是这两个数字的二进制数对应位不同的数量。
+ * <p>
+ * 计算一个数组中，任意两个数之间汉明距离的总和。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 4, 14, 2
+ * <p>
+ * 输出: 6
+ * <p>
+ * 解释: 在二进制表示中，4表示为0100，14表示为1110，2表示为0010。（这样表示是为了体现后四位之间关系）
+ * 所以答案为：
+ * HammingDistance(4, 14) + HammingDistance(4, 2) + HammingDistance(14, 2) = 2 + 2 + 2 = 6.
+ * 注意:
+ * <p>
+ * 数组中元素的范围为从 0到 10^9。
+ * 数组的长度不超过 10^4。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/total-hamming-distance
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author yi
+ * @Date 2021/5/28 22:17
+ */
+public class TotalHammingDistance {
+}
+
+
+class Solution {
+    public int totalHammingDistance(int[] nums) {
+        int ans = 0, n = nums.length;
+        for (int i = 0; i < 30; ++i) {
+            int c = 0;
+            for (int val : nums) {
+                c += (val >> i) & 1;
+            }
+            ans += c * (n - c);
+        }
+        return ans;
+    }
+}
