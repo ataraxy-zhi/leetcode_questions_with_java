45a147c32cd376f60a859f2874841286db2ffda9
ataraxy-zhi <2711059631@qq.com>
2021-05-31 22:55:46 +0800

    342. Power of Four

---
+++ src/com/solution/onequestionperday/may2021/poweroffour342/PowerOfFour.java
@@ -0,0 +1,71 @@
+package com.solution.onequestionperday.may2021.poweroffour342;
+
+/**
+ * @ClassName PowerOfFour
+ * @Description 342. Power of Four
+ * Given an integer n, return true if it is a power of four. Otherwise, return false.
+ * <p>
+ * An integer n is a power of four, if there exists an integer x such that n == 4x.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 16
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input: n = 5
+ * Output: false
+ * Example 3:
+ * <p>
+ * Input: n = 1
+ * Output: true
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * -231 <= n <= 231 - 1
+ * <p>
+ * <p>
+ * Follow up: Could you solve it without loops/recursion?
+ * <p>
+ * <p>
+ * 342. 4的幂
+ * 给定一个整数，写一个函数来判断它是否是 4 的幂次方。如果是，返回 true ；否则，返回 false 。
+ * <p>
+ * 整数 n 是 4 的幂次方需满足：存在整数 x 使得 n == 4x
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 16
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * 输入：n = 5
+ * 输出：false
+ * 示例 3：
+ * <p>
+ * 输入：n = 1
+ * 输出：true
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * -231 <= n <= 231 - 1
+ *  
+ * <p>
+ * 进阶：
+ * <p>
+ * 你能不使用循环或者递归来完成本题吗？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/power-of-four
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author yi
+ * @Date 2021/5/31 22:54
+ */
+public class PowerOfFour {
+}
