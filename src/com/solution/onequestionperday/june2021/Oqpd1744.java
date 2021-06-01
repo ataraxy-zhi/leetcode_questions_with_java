1e1fa67f5e79ccf0e53fddc31df5f91d8ec90be4
ataraxy-zhi <2711059631@qq.com>
2021-06-01 22:21:35 +0800

    1744. Can You Eat Your Favorite Candy on Your Favorite Day?

---
+++ src/com/solution/onequestionperday/june2021/Oqpd1744.java
@@ -0,0 +1,119 @@
+package com.solution.onequestionperday.june2021;
+
+/**
+ * @ClassName Oqpd1744
+ * @Description 1744. Can You Eat Your Favorite Candy on Your Favorite Day?
+ * You are given a (0-indexed) array of positive integers candiesCount where candiesCount[i] represents the number of candies of the ith type you have. You are also given a 2D array queries where queries[i] = [favoriteTypei, favoriteDayi, dailyCapi].
+ * <p>
+ * You play a game with the following rules:
+ * <p>
+ * You start eating candies on day 0.
+ * You cannot eat any candy of type i unless you have eaten all candies of type i - 1.
+ * You must eat at least one candy per day until you have eaten all the candies.
+ * Construct a boolean array answer such that answer.length == queries.length and answer[i] is true if you can eat a candy of type favoriteTypei on day favoriteDayi without eating more than dailyCapi candies on any day, and false otherwise. Note that you can eat different types of candy on the same day, provided that you follow rule 2.
+ * <p>
+ * Return the constructed array answer.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: candiesCount = [7,4,5,3,8], queries = [[0,2,2],[4,2,4],[2,13,1000000000]]
+ * Output: [true,false,true]
+ * Explanation:
+ * 1- If you eat 2 candies (type 0) on day 0 and 2 candies (type 0) on day 1, you will eat a candy of type 0 on day 2.
+ * 2- You can eat at most 4 candies each day.
+ * If you eat 4 candies every day, you will eat 4 candies (type 0) on day 0 and 4 candies (type 0 and type 1) on day 1.
+ * On day 2, you can only eat 4 candies (type 1 and type 2), so you cannot eat a candy of type 4 on day 2.
+ * 3- If you eat 1 candy each day, you will eat a candy of type 2 on day 13.
+ * Example 2:
+ * <p>
+ * Input: candiesCount = [5,2,6,4,1], queries = [[3,1,2],[4,10,3],[3,10,100],[4,100,30],[1,3,1]]
+ * Output: [false,true,true,false,false]
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= candiesCount.length <= 105
+ * 1 <= candiesCount[i] <= 105
+ * 1 <= queries.length <= 105
+ * queries[i].length == 3
+ * 0 <= favoriteTypei < candiesCount.length
+ * 0 <= favoriteDayi <= 109
+ * 1 <= dailyCapi <= 109
+ * <p>
+ * 1744. 你能在你最喜欢的那天吃到你最喜欢的糖果吗？
+ * 给你一个下标从 0 开始的正整数数组 candiesCount ，其中 candiesCount[i] 表示你拥有的第 i 类糖果的数目。同时给你一个二维数组 queries ，其中 queries[i] = [favoriteTypei, favoriteDayi, dailyCapi] 。
+ * <p>
+ * 你按照如下规则进行一场游戏：
+ * <p>
+ * 你从第 0 天开始吃糖果。
+ * 你在吃完 所有 第 i - 1 类糖果之前，不能 吃任何一颗第 i 类糖果。
+ * 在吃完所有糖果之前，你必须每天 至少 吃 一颗 糖果。
+ * 请你构建一个布尔型数组 answer ，满足 answer.length == queries.length 。answer[i] 为 true 的条件是：在每天吃 不超过 dailyCapi 颗糖果的前提下，你可以在第 favoriteDayi 天吃到第 favoriteTypei 类糖果；否则 answer[i] 为 false 。注意，只要满足上面 3 条规则中的第二条规则，你就可以在同一天吃不同类型的糖果。
+ * <p>
+ * 请你返回得到的数组 answer 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：candiesCount = [7,4,5,3,8], queries = [[0,2,2],[4,2,4],[2,13,1000000000]]
+ * 输出：[true,false,true]
+ * 提示：
+ * 1- 在第 0 天吃 2 颗糖果(类型 0），第 1 天吃 2 颗糖果（类型 0），第 2 天你可以吃到类型 0 的糖果。
+ * 2- 每天你最多吃 4 颗糖果。即使第 0 天吃 4 颗糖果（类型 0），第 1 天吃 4 颗糖果（类型 0 和类型 1），你也没办法在第 2 天吃到类型 4 的糖果。换言之，你没法在每天吃 4 颗糖果的限制下在第 2 天吃到第 4 类糖果。
+ * 3- 如果你每天吃 1 颗糖果，你可以在第 13 天吃到类型 2 的糖果。
+ * 示例 2：
+ * <p>
+ * 输入：candiesCount = [5,2,6,4,1], queries = [[3,1,2],[4,10,3],[3,10,100],[4,100,30],[1,3,1]]
+ * 输出：[false,true,true,false,false]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= candiesCount.length <= 105
+ * 1 <= candiesCount[i] <= 105
+ * 1 <= queries.length <= 105
+ * queries[i].length == 3
+ * 0 <= favoriteTypei < candiesCount.length
+ * 0 <= favoriteDayi <= 109
+ * 1 <= dailyCapi <= 109
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/can-you-eat-your-favorite-candy-on-your-favorite-day
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author yi
+ * @Date 2021/6/1 22:20
+ */
+public class Oqpd1744 {
+}
+
+
+class Solution {
+    public boolean[] canEat(int[] candiesCount, int[][] queries) {
+        int n = candiesCount.length;
+
+        // 前缀和
+        long[] sum = new long[n];
+        sum[0] = candiesCount[0];
+        for (int i = 1; i < n; ++i) {
+            sum[i] = sum[i - 1] + candiesCount[i];
+        }
+
+        int q = queries.length;
+        boolean[] ans = new boolean[q];
+        for (int i = 0; i < q; ++i) {
+            int[] query = queries[i];
+            int favoriteType = query[0], favoriteDay = query[1], dailyCap = query[2];
+
+            long x1 = favoriteDay + 1;
+            long y1 = (long) (favoriteDay + 1) * dailyCap;
+            long x2 = favoriteType == 0 ? 1 : sum[favoriteType - 1] + 1;
+            long y2 = sum[favoriteType];
+
+            ans[i] = !(x1 > y2 || y1 < x2);
+        }
+        return ans;
+    }
+}
