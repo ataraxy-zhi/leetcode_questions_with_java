2d7a9d857d225a23a399cf612605d81acba313be
ataraxy-zhi <2711059631@qq.com>
2021-06-08 23:00:18 +0800

    1049. Last Stone Weight II

---
+++ src/com/solution/onequestionperday/june2021/oqpd1049/Oqpd1049.java
@@ -0,0 +1,106 @@
+package com.solution.onequestionperday.june2021.oqpd1049;
+
+/**
+ * @ClassName Oqpd1049
+ * @Description 1049. Last Stone Weight II
+ * You are given an array of integers stones where stones[i] is the weight of the ith stone.
+ * <p>
+ * We are playing a game with the stones. On each turn, we choose any two stones and smash them together. Suppose the stones have weights x and y with x <= y. The result of this smash is:
+ * <p>
+ * If x == y, both stones are destroyed, and
+ * If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+ * At the end of the game, there is at most one stone left.
+ * <p>
+ * Return the smallest possible weight of the left stone. If there are no stones left, return 0.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: stones = [2,7,4,1,8,1]
+ * Output: 1
+ * Explanation:
+ * We can combine 2 and 4 to get 2, so the array converts to [2,7,1,8,1] then,
+ * we can combine 7 and 8 to get 1, so the array converts to [2,1,1,1] then,
+ * we can combine 2 and 1 to get 1, so the array converts to [1,1,1] then,
+ * we can combine 1 and 1 to get 0, so the array converts to [1], then that's the optimal value.
+ * Example 2:
+ * <p>
+ * Input: stones = [31,26,33,21,40]
+ * Output: 5
+ * Example 3:
+ * <p>
+ * Input: stones = [1,2]
+ * Output: 1
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= stones.length <= 30
+ * 1 <= stones[i] <= 100
+ * 1049. 最后一块石头的重量 II
+ * 有一堆石头，用整数数组 stones 表示。其中 stones[i] 表示第 i 块石头的重量。
+ * <p>
+ * 每一回合，从中选出任意两块石头，然后将它们一起粉碎。假设石头的重量分别为 x 和 y，且 x <= y。那么粉碎的可能结果如下：
+ * <p>
+ * 如果 x == y，那么两块石头都会被完全粉碎；
+ * 如果 x != y，那么重量为 x 的石头将会完全粉碎，而重量为 y 的石头新重量为 y-x。
+ * 最后，最多只会剩下一块 石头。返回此石头 最小的可能重量 。如果没有石头剩下，就返回 0。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：stones = [2,7,4,1,8,1]
+ * 输出：1
+ * 解释：
+ * 组合 2 和 4，得到 2，所以数组转化为 [2,7,1,8,1]，
+ * 组合 7 和 8，得到 1，所以数组转化为 [2,1,1,1]，
+ * 组合 2 和 1，得到 1，所以数组转化为 [1,1,1]，
+ * 组合 1 和 1，得到 0，所以数组转化为 [1]，这就是最优值。
+ * 示例 2：
+ * <p>
+ * 输入：stones = [31,26,33,21,40]
+ * 输出：5
+ * 示例 3：
+ * <p>
+ * 输入：stones = [1,2]
+ * 输出：1
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= stones.length <= 30
+ * 1 <= stones[i] <= 100
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/last-stone-weight-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author yi
+ * @Date 2021/6/8 22:56
+ */
+public class Oqpd1049 {
+}
+
+
+class Solution {
+    public int lastStoneWeightII(int[] stones) {
+        int sum = 0;
+        for (int weight : stones) {
+            sum += weight;
+        }
+        int m = sum / 2;
+        boolean[] dp = new boolean[m + 1];
+        dp[0] = true;
+        for (int weight : stones) {
+            for (int j = m; j >= weight; --j) {
+                dp[j] = dp[j] || dp[j - weight];
+            }
+        }
+        for (int j = m; ; --j) {
+            if (dp[j]) {
+                return sum - 2 * j;
+            }
+        }
+    }
+}
