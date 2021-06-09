2fdb9ac2f8aeefd2e5fb32a47e20952cc087b085
ataraxy-zhi <2711059631@qq.com>
2021-06-09 21:06:11 +0800

    879. Profitable Schemes

---
+++ src/com/solution/onequestionperday/june2021/oqpd879/Oqpd879.java
@@ -0,0 +1,103 @@
+package com.solution.onequestionperday.june2021.oqpd879;
+
+/**
+ * @ClassName Oqpd879
+ * @Description 879. Profitable Schemes
+ * There is a group of n members, and a list of various crimes they could commit. The ith crime generates a profit[i] and requires group[i] members to participate in it. If a member participates in one crime, that member can't participate in another crime.
+ * <p>
+ * Let's call a profitable scheme any subset of these crimes that generates at least minProfit profit, and the total number of members participating in that subset of crimes is at most n.
+ * <p>
+ * Return the number of schemes that can be chosen. Since the answer may be very large, return it modulo 109 + 7.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 5, minProfit = 3, group = [2,2], profit = [2,3]
+ * Output: 2
+ * Explanation: To make a profit of at least 3, the group could either commit crimes 0 and 1, or just crime 1.
+ * In total, there are 2 schemes.
+ * Example 2:
+ * <p>
+ * Input: n = 10, minProfit = 5, group = [2,3,5], profit = [6,7,8]
+ * Output: 7
+ * Explanation: To make a profit of at least 5, the group could commit any crimes, as long as they commit one.
+ * There are 7 possible schemes: (0), (1), (2), (0,1), (0,2), (1,2), and (0,1,2).
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= n <= 100
+ * 0 <= minProfit <= 100
+ * 1 <= group.length <= 100
+ * 1 <= group[i] <= 100
+ * profit.length == group.length
+ * 0 <= profit[i] <= 100
+ * <p>
+ * 879. 盈利计划
+ * 集团里有 n 名员工，他们可以完成各种各样的工作创造利润。
+ * <p>
+ * 第 i 种工作会产生 profit[i] 的利润，它要求 group[i] 名成员共同参与。如果成员参与了其中一项工作，就不能参与另一项工作。
+ * <p>
+ * 工作的任何至少产生 minProfit 利润的子集称为 盈利计划 。并且工作的成员总数最多为 n 。
+ * <p>
+ * 有多少种计划可以选择？因为答案很大，所以 返回结果模 10^9 + 7 的值。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 5, minProfit = 3, group = [2,2], profit = [2,3]
+ * 输出：2
+ * 解释：至少产生 3 的利润，该集团可以完成工作 0 和工作 1 ，或仅完成工作 1 。
+ * 总的来说，有两种计划。
+ * 示例 2：
+ * <p>
+ * 输入：n = 10, minProfit = 5, group = [2,3,5], profit = [6,7,8]
+ * 输出：7
+ * 解释：至少产生 5 的利润，只要完成其中一种工作就行，所以该集团可以完成任何工作。
+ * 有 7 种可能的计划：(0)，(1)，(2)，(0,1)，(0,2)，(1,2)，以及 (0,1,2) 。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= n <= 100
+ * 0 <= minProfit <= 100
+ * 1 <= group.length <= 100
+ * 1 <= group[i] <= 100
+ * profit.length == group.length
+ * 0 <= profit[i] <= 100
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/profitable-schemes
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author yi
+ * @Date 2021/6/9 21:03
+ */
+public class Oqpd879 {
+}
+
+class Solution {
+    public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
+        int len = group.length, mod = (int) 1e9 + 7;
+        int[][][] dp = new int[len + 1][n + 1][minProfit + 1];
+        dp[0][0][0] = 1;
+        for (int i = 1; i <= len; i++) {
+            int members = group[i - 1], earn = profit[i - 1];
+            for (int j = 0; j <= n; j++) {
+                for (int k = 0; k <= minProfit; k++) {
+                    if (j < members) {
+                        dp[i][j][k] = dp[i - 1][j][k];
+                    } else {
+                        dp[i][j][k] = (dp[i - 1][j][k] + dp[i - 1][j - members][Math.max(0, k - earn)]) % mod;
+                    }
+                }
+            }
+        }
+        int sum = 0;
+        for (int j = 0; j <= n; j++) {
+            sum = (sum + dp[len][j][minProfit]) % mod;
+        }
+        return sum;
+    }
+}
