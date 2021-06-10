ab979c04f1034e431c59419c8660a8c792ce0789
ataraxy-zhi <2711059631@qq.com>
2021-06-10 22:29:03 +0800

    518. Coin Change 2

---
+++ src/com/solution/onequestionperday/june2021/oqpd518/Oqpd518.java
@@ -0,0 +1,96 @@
+package com.solution.onequestionperday.june2021.oqpd518;
+
+/**
+ * @ClassName Oqpd518
+ * @Description 518. Coin Change 2
+ * You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+ * <p>
+ * Return the number of combinations that make up that amount. If that amount of money cannot be made up by any combination of the coins, return 0.
+ * <p>
+ * You may assume that you have an infinite number of each kind of coin.
+ * <p>
+ * The answer is guaranteed to fit into a signed 32-bit integer.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: amount = 5, coins = [1,2,5]
+ * Output: 4
+ * Explanation: there are four ways to make up the amount:
+ * 5=5
+ * 5=2+2+1
+ * 5=2+1+1+1
+ * 5=1+1+1+1+1
+ * Example 2:
+ * <p>
+ * Input: amount = 3, coins = [2]
+ * Output: 0
+ * Explanation: the amount of 3 cannot be made up just with coins of 2.
+ * Example 3:
+ * <p>
+ * Input: amount = 10, coins = [10]
+ * Output: 1
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= coins.length <= 300
+ * 1 <= coins[i] <= 5000
+ * All the values of coins are unique.
+ * 0 <= amount <= 5000
+ * 518. 零钱兑换 II
+ * 给定不同面额的硬币和一个总金额。写出函数来计算可以凑成总金额的硬币组合数。假设每一种面额的硬币有无限个。 
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: amount = 5, coins = [1, 2, 5]
+ * 输出: 4
+ * 解释: 有四种方式可以凑成总金额:
+ * 5=5
+ * 5=2+2+1
+ * 5=2+1+1+1
+ * 5=1+1+1+1+1
+ * 示例 2:
+ * <p>
+ * 输入: amount = 3, coins = [2]
+ * 输出: 0
+ * 解释: 只用面额2的硬币不能凑成总金额3。
+ * 示例 3:
+ * <p>
+ * 输入: amount = 10, coins = [10]
+ * 输出: 1
+ *  
+ * <p>
+ * 注意:
+ * <p>
+ * 你可以假设：
+ * <p>
+ * 0 <= amount (总金额) <= 5000
+ * 1 <= coin (硬币面额) <= 5000
+ * 硬币种类不超过 500 种
+ * 结果符合 32 位符号整数
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/coin-change-2
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author yi
+ * @Date 2021/6/10 22:27
+ */
+public class Oqpd518 {
+}
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+}
