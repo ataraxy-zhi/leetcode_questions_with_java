227b301153328a4ea8e9f0f50db9142b01a96e2e
ataraxy-zhi <2711059631@qq.com>
2021-06-12 22:14:10 +0800

    1449. Form Largest Integer With Digits That Add up to Target

---
+++ src/com/solution/onequestionperday/june2021/oqpd1449/Oqpd1449.java
@@ -0,0 +1,154 @@
+package com.solution.onequestionperday.june2021.oqpd1449;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName Oqpd1449
+ * @Description 1449. Form Largest Integer With Digits That Add up to Target
+ * Given an array of integers cost and an integer target. Return the maximum integer you can paint under the following rules:
+ * <p>
+ * The cost of painting a digit (i+1) is given by cost[i] (0 indexed).
+ * The total cost used must be equal to target.
+ * Integer does not have digits 0.
+ * Since the answer may be too large, return it as string.
+ * <p>
+ * If there is no way to paint any integer given the condition, return "0".
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: cost = [4,3,2,5,6,7,2,5,5], target = 9
+ * Output: "7772"
+ * Explanation:  The cost to paint the digit '7' is 2, and the digit '2' is 3. Then cost("7772") = 2*3+ 3*1 = 9. You could also paint "977", but "7772" is the largest number.
+ * Digit    cost
+ * 1  ->   4
+ * 2  ->   3
+ * 3  ->   2
+ * 4  ->   5
+ * 5  ->   6
+ * 6  ->   7
+ * 7  ->   2
+ * 8  ->   5
+ * 9  ->   5
+ * Example 2:
+ * <p>
+ * Input: cost = [7,6,5,5,5,6,8,7,8], target = 12
+ * Output: "85"
+ * Explanation: The cost to paint the digit '8' is 7, and the digit '5' is 5. Then cost("85") = 7 + 5 = 12.
+ * Example 3:
+ * <p>
+ * Input: cost = [2,4,6,2,4,6,4,4,4], target = 5
+ * Output: "0"
+ * Explanation: It's not possible to paint any integer with total cost equal to target.
+ * Example 4:
+ * <p>
+ * Input: cost = [6,10,15,40,40,40,40,40,40], target = 47
+ * Output: "32211"
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * cost.length == 9
+ * 1 <= cost[i] <= 5000
+ * 1 <= target <= 5000
+ * 1449. 数位成本和为目标值的最大数字
+ * 给你一个整数数组 cost 和一个整数 target 。请你返回满足如下规则可以得到的 最大 整数：
+ * <p>
+ * 给当前结果添加一个数位（i + 1）的成本为 cost[i] （cost 数组下标从 0 开始）。
+ * 总成本必须恰好等于 target 。
+ * 添加的数位中没有数字 0 。
+ * 由于答案可能会很大，请你以字符串形式返回。
+ * <p>
+ * 如果按照上述要求无法得到任何整数，请你返回 "0" 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：cost = [4,3,2,5,6,7,2,5,5], target = 9
+ * 输出："7772"
+ * 解释：添加数位 '7' 的成本为 2 ，添加数位 '2' 的成本为 3 。所以 "7772" 的代价为 2*3+ 3*1 = 9 。 "977" 也是满足要求的数字，但 "7772" 是较大的数字。
+ * 数字     成本
+ * 1  ->   4
+ * 2  ->   3
+ * 3  ->   2
+ * 4  ->   5
+ * 5  ->   6
+ * 6  ->   7
+ * 7  ->   2
+ * 8  ->   5
+ * 9  ->   5
+ * 示例 2：
+ * <p>
+ * 输入：cost = [7,6,5,5,5,6,8,7,8], target = 12
+ * 输出："85"
+ * 解释：添加数位 '8' 的成本是 7 ，添加数位 '5' 的成本是 5 。"85" 的成本为 7 + 5 = 12 。
+ * 示例 3：
+ * <p>
+ * 输入：cost = [2,4,6,2,4,6,4,4,4], target = 5
+ * 输出："0"
+ * 解释：总成本是 target 的条件下，无法生成任何整数。
+ * 示例 4：
+ * <p>
+ * 输入：cost = [6,10,15,40,40,40,40,40,40], target = 47
+ * 输出："32211"
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * cost.length == 9
+ * 1 <= cost[i] <= 5000
+ * 1 <= target <= 5000
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/form-largest-integer-with-digits-that-add-up-to-target
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author yi
+ * @Date 2021/6/12 22:12
+ */
+public class Oqpd1449 {
+}
+
+
+class Solution {
+    public String largestNumber(int[] cost, int target) {
+        int[][] dp = new int[10][target + 1];
+        for (int i = 0; i < 10; ++i) {
+            Arrays.fill(dp[i], Integer.MIN_VALUE);
+        }
+        int[][] from = new int[10][target + 1];
+        dp[0][0] = 0;
+        for (int i = 0; i < 9; ++i) {
+            int c = cost[i];
+            for (int j = 0; j <= target; ++j) {
+                if (j < c) {
+                    dp[i + 1][j] = dp[i][j];
+                    from[i + 1][j] = j;
+                } else {
+                    if (dp[i][j] > dp[i + 1][j - c] + 1) {
+                        dp[i + 1][j] = dp[i][j];
+                        from[i + 1][j] = j;
+                    } else {
+                        dp[i + 1][j] = dp[i + 1][j - c] + 1;
+                        from[i + 1][j] = j - c;
+                    }
+                }
+            }
+        }
+        if (dp[9][target] < 0) {
+            return "0";
+        }
+        StringBuffer sb = new StringBuffer();
+        int i = 9, j = target;
+        while (i > 0) {
+            if (j == from[i][j]) {
+                --i;
+            } else {
+                sb.append(i);
+                j = from[i][j];
+            }
+        }
+        return sb.toString();
+    }
+}
