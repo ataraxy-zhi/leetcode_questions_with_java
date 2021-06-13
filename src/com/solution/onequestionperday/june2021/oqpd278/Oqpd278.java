dc189599128f2aced0aad8ad317e5bc21e2a80f8
ataraxy-zhi <2711059631@qq.com>
2021-06-13 23:06:25 +0800

    278. First Bad Version

---
+++ src/com/solution/onequestionperday/june2021/oqpd278/Oqpd278.java
@@ -0,0 +1,73 @@
+package com.solution.onequestionperday.june2021.oqpd278;
+
+/**
+ * @ClassName Oqpd278
+ * @Description 278. First Bad Version
+ * You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+ * <p>
+ * Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+ * <p>
+ * You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 5, bad = 4
+ * Output: 4
+ * Explanation:
+ * call isBadVersion(3) -> false
+ * call isBadVersion(5) -> true
+ * call isBadVersion(4) -> true
+ * Then 4 is the first bad version.
+ * Example 2:
+ * <p>
+ * Input: n = 1, bad = 1
+ * Output: 1
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= bad <= n <= 231 - 1
+ * 278. 第一个错误的版本
+ * 你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+ * <p>
+ * 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+ * <p>
+ * 你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 n = 5，并且 version = 4 是第一个错误的版本。
+ * <p>
+ * 调用 isBadVersion(3) -> false
+ * 调用 isBadVersion(5) -> true
+ * 调用 isBadVersion(4) -> true
+ * <p>
+ * 所以，4 是第一个错误的版本。 
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/first-bad-version
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author yi
+ * @Date 2021/6/13 23:04
+ */
+public class Oqpd278 {
+}
+
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right) { // 循环直至区间左右端点相同
+            int mid = left + (right - left) / 2; // 防止计算时溢出
+            if (isBadVersion(mid)) {
+                right = mid; // 答案在区间 [left, mid] 中
+            } else {
+                left = mid + 1; // 答案在区间 [mid+1, right] 中
+            }
+        }
+        // 此时有 left == right，区间缩为一个点，即为答案
+        return left;
+    }
+}
