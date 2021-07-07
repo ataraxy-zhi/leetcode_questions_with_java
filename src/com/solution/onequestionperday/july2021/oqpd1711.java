29889ec5737f8e212ed841ccaa8370fb58dc974c
ataraxy-zhi <2711059631@qq.com>
2021-07-07 21:32:33 +0800

    1711. 大餐计数

---
+++ src/com/solution/onequestionperday/july2021/oqpd1711.java
@@ -0,0 +1,67 @@
+package com.solution.onequestionperday.july2021;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName oqpd1711
+ * @Description 1711. 大餐计数
+ * 大餐 是指 恰好包含两道不同餐品 的一餐，其美味程度之和等于 2 的幂。
+ * <p>
+ * 你可以搭配 任意 两道餐品做一顿大餐。
+ * <p>
+ * 给你一个整数数组 deliciousness ，其中 deliciousness[i] 是第 i​​​​​​​​​​​​​​ 道餐品的美味程度，返回你可以用数组中的餐品做出的不同 大餐 的数量。结果需要对 109 + 7 取余。
+ * <p>
+ * 注意，只要餐品下标不同，就可以认为是不同的餐品，即便它们的美味程度相同。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：deliciousness = [1,3,5,7,9]
+ * 输出：4
+ * 解释：大餐的美味程度组合为 (1,3) 、(1,7) 、(3,5) 和 (7,9) 。
+ * 它们各自的美味程度之和分别为 4 、8 、8 和 16 ，都是 2 的幂。
+ * 示例 2：
+ * <p>
+ * 输入：deliciousness = [1,1,1,3,3,3,7]
+ * 输出：15
+ * 解释：大餐的美味程度组合为 3 种 (1,1) ，9 种 (1,3) ，和 3 种 (1,7) 。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= deliciousness.length <= 105
+ * 0 <= deliciousness[i] <= 220
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/count-good-meals
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author yi
+ * @Date 2021/7/7 21:30
+ */
+public class oqpd1711 {
+}
+
+
+class Solution {
+    public int countPairs(int[] deliciousness) {
+        final int MOD = 1000000007;
+        int maxVal = 0;
+        for (int val : deliciousness) {
+            maxVal = Math.max(maxVal, val);
+        }
+        int maxSum = maxVal * 2;
+        int pairs = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = deliciousness.length;
+        for (int val : deliciousness) {
+            for (int sum = 1; sum <= maxSum; sum <<= 1) {
+                int count = map.getOrDefault(sum - val, 0);
+                pairs = (pairs + count) % MOD;
+            }
+            map.put(val, map.getOrDefault(val, 0) + 1);
+        }
+        return pairs;
+    }
+}
