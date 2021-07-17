a5574e06b1ab8dd1e506d91c3af3ddbf738a2a1d
ataraxy-zhi <2711059631@qq.com>
2021-07-17 15:24:13 +0800

    1846. 减小和重新排列数组后的最大元素

---
+++ src/com/solution/onequestionperday/july2021/psto42/Psto42.java
@@ -0,0 +1,48 @@
+package com.solution.onequestionperday.july2021.psto42;
+
+/**
+ * @ClassName Psto42
+ * @Description 剑指 Offer 42. 连续子数组的最大和
+ * 输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+ * <p>
+ * 要求时间复杂度为O(n)。
+ * <p>
+ *  
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= arr.length <= 10^5
+ * -100 <= arr[i] <= 100
+ * 注意：本题与主站 53 题相同：https://leetcode-cn.com/problems/maximum-subarray/
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author yi
+ * @Date 2021/7/17 15:23
+ */
+public class Psto42 {
+}
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int temp = 0;
+        int maxSum = nums[0];
+
+        for (int num : nums) {
+            temp = Math.max(temp + num, num);
+            maxSum = Math.max(maxSum, temp);
+        }
+
+        return maxSum;
+
+    }
+}
