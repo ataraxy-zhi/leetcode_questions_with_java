d8d9dcf38b22f516a49ef81e2efe650c153e9c87
ataraxy-zhi <2711059631@qq.com>
2021-07-24 16:20:51 +0800

    1736. 替换隐藏数字得到的最晚时间

---
+++ src/com/solution/onequestionperday/july2021/oqpd1736/Oqpd1736.java
@@ -0,0 +1,59 @@
+package com.solution.onequestionperday.july2021.oqpd1736;
+
+/**
+ * @Description 1736. 替换隐藏数字得到的最晚时间
+ * 给你一个字符串 time ，格式为 hh:mm（小时：分钟），其中某几位数字被隐藏（用 ? 表示）。
+ * <p>
+ * 有效的时间为 00:00 到 23:59 之间的所有时间，包括 00:00 和 23:59 。
+ * <p>
+ * 替换 time 中隐藏的数字，返回你可以得到的最晚有效时间。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：time = "2?:?0"
+ * 输出："23:50"
+ * 解释：以数字 '2' 开头的最晚一小时是 23 ，以 '0' 结尾的最晚一分钟是 50 。
+ * 示例 2：
+ * <p>
+ * 输入：time = "0?:3?"
+ * 输出："09:39"
+ * 示例 3：
+ * <p>
+ * 输入：time = "1?:22"
+ * 输出："19:22"
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * time 的格式为 hh:mm
+ * 题目数据保证你可以由输入的字符串生成有效的时间
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/latest-time-by-replacing-hidden-digits
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author yi
+ * @Date 2021/7/24 16:20
+ */
+public class Oqpd1736 {
+}
+
+class Solution {
+    public String maximumTime(String time) {
+        char[] arr = time.toCharArray();
+        if (arr[0] == '?') {
+            arr[0] = ('4' <= arr[1] && arr[1] <= '9') ? '1' : '2';
+        }
+        if (arr[1] == '?') {
+            arr[1] = (arr[0] == '2') ? '3' : '9';
+        }
+        if (arr[3] == '?') {
+            arr[3] = '5';
+        }
+        if (arr[4] == '?') {
+            arr[4] = '9';
+        }
+        return new String(arr);
+    }
+}
