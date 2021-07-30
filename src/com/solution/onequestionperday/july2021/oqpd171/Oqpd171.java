097c84e81dc85709a34bf23bb29eeefb9f8bbdc7
ataraxy-zhi <2711059631@qq.com>
2021-07-30 21:31:12 +0800

    171. Excel 表列序号

---
+++ src/com/solution/onequestionperday/july2021/oqpd171/Oqpd171.java
@@ -0,0 +1,64 @@
+package com.solution.onequestionperday.july2021.oqpd171;
+
+/**
+ * 171. Excel 表列序号
+ * 给你一个字符串 columnTitle ，表示 Excel 表格中的列名称。返回该列名称对应的列序号。
+ * <p>
+ *  
+ * <p>
+ * 例如，
+ * <p>
+ * A -> 1
+ * B -> 2
+ * C -> 3
+ * ...
+ * Z -> 26
+ * AA -> 27
+ * AB -> 28
+ * ...
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: columnTitle = "A"
+ * 输出: 1
+ * 示例 2:
+ * <p>
+ * 输入: columnTitle = "AB"
+ * 输出: 28
+ * 示例 3:
+ * <p>
+ * 输入: columnTitle = "ZY"
+ * 输出: 701
+ * 示例 4:
+ * <p>
+ * 输入: columnTitle = "FXSHRXW"
+ * 输出: 2147483647
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= columnTitle.length <= 7
+ * columnTitle 仅由大写英文组成
+ * columnTitle 在范围 ["A", "FXSHRXW"] 内
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/excel-sheet-column-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author yi
+ * @date 2021/7/30 21:30
+ */
+public class Oqpd171 {
+}
+
+class Solution {
+    public int titleToNumber(String s) {
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int num = s.charAt(i) - 'A' + 1;
+            ans = ans * 26 + num;
+        }
+        return ans;
+    }
+}
