cdb734d51657a1f945592ba8b2066264da0db570
ataraxy-zhi <2711059631@qq.com>
2021-09-05 18:59:40 +0800

    5864. 游戏中弱角色的数量

---
+++ src/com/solution/medium/Contest257II.java
@@ -0,0 +1,75 @@
+package com.solution.medium;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 5864. 游戏中弱角色的数量
+ * 你正在参加一个多角色游戏，每个角色都有两个主要属性：攻击 和 防御 。给你一个二维整数数组 properties ，其中 properties[i] = [attacki, defensei] 表示游戏中第 i 个角色的属性。
+ * <p>
+ * 如果存在一个其他角色的攻击和防御等级 都严格高于 该角色的攻击和防御等级，则认为该角色为 弱角色 。更正式地，如果认为角色 i 弱于 存在的另一个角色 j ，那么 attackj > attacki 且 defensej > defensei 。
+ * <p>
+ * 返回 弱角色 的数量。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：properties = [[5,5],[6,3],[3,6]]
+ * 输出：0
+ * 解释：不存在攻击和防御都严格高于其他角色的角色。
+ * 示例 2：
+ * <p>
+ * 输入：properties = [[2,2],[3,3]]
+ * 输出：1
+ * 解释：第一个角色是弱角色，因为第二个角色的攻击和防御严格大于该角色。
+ * 示例 3：
+ * <p>
+ * 输入：properties = [[1,5],[10,4],[4,3]]
+ * 输出：1
+ * 解释：第三个角色是弱角色，因为第二个角色的攻击和防御严格大于该角色。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 2 <= properties.length <= 105
+ * properties[i].length == 2
+ * 1 <= attacki, defensei <= 105
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/the-number-of-weak-characters-in-the-game
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author yi
+ * @date 2021/9/5 18:44
+ */
+public class Contest257II {
+
+}
+
+
+class Solution {
+    public int numberOfWeakCharacters(int[][] properties) {
+        int n = properties.length;
+
+        Arrays.sort(properties, (o1, o2) -> {
+            if (o1[0] != o2[0]) {
+                return o1[0] - o2[0];
+            } else {
+                return o2[1] - o1[1];
+            }
+        });
+
+        int res = 0;
+        int right = -1;
+        for (int i = n - 1; i > -1; i--) {
+            int x = properties[i][0], y = properties[i][1];
+            if (y < right) {
+                res++;
+            }
+            right = Math.max(right, y);
+        }
+
+        return res;
+    }
+}
